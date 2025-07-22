2758d08b129ac44e0b12277dddc55acfb1b70047
Devidas Zende <devzende27@gmail.com>
2025-07-22 15:27:37 +0530

    Add files via upload
    uploaded all required files

---
+++ vault.js
@@ -0,0 +1,9 @@
+// Lock vault button
+document.getElementById('lock-vault').addEventListener('click', () => {
+    window.location.href = 'index.html';
+});
+
+// Change pattern button
+document.getElementById('change-pattern').addEventListener('click', () => {
+    window.location.href = 'change-password.html';
+});
